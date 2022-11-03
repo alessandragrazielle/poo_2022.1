5b148770cc6b9ac436558e775b6e3b37166326c4
Alessandra Grazielle <61811422+alessandragrazielle@users.noreply.github.com>
2022-11-03 12:26:48 -0300

    Update and rename questoes03e06.ts to questoes03a06.ts

--- atv_excecoes01/questoes03a06.ts
+++ atv_excecoes01/questoes03a06.ts
@@ -38,32 +38,6 @@ class Conta{
     }
 }
 
-class Banco{
-    private _contas: Conta[] = [];
-
-    inserir(conta: Conta) {
-        this._contas.push(conta);
-    }
-
-    consultarPorIndice(numero: string): number {
-        let indiceProcurado: number = -1;
-        for (let i = 0; i < this._contas.length; i++) {
-            if (this._contas[i].numero == numero) {
-                indiceProcurado = i;
-            }
-        }
-
-        return indiceProcurado;
-    }
-
-    sacar(numero: string, valor: number): void {
-        let indice = this.consultarPorIndice(numero);
-
-        if (indice != -1) {
-            this._contas[indice].sacar(valor);
-        }
-    }
-}
 
 let c1: Conta = new Conta('1', 150);
 //c1.sacar(300);
@@ -72,14 +46,3 @@ let c2: Conta = new Conta('2', 300);
 //c1.transferir(c2, 200);
 //c2.depositar(-100);
 console.log(`Saldo conta 1: ${c1.saldo} \nSaldo conta 2: ${c2.saldo}`);
-
-
-let c3: Conta = new Conta('3', 500);
-let c4: Conta = new Conta('4', 600);
-
-let b1: Banco = new Banco();
-b1.inserir(c3);
-b1.inserir(c4);
-
-b1.sacar('c3', 600);
-console.log(`Saldo conta 3: ${c3.saldo} \nSaldo conta 2: ${c4.saldo}`)
