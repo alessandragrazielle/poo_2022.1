aae1d52087bde257bb7393f377344787a6ebe721
Alessandra Grazielle <61811422+alessandragrazielle@users.noreply.github.com>
2022-12-10 11:59:27 -0300

    Update index.ts

--- trabalhoFinal/index.ts
+++ trabalhoFinal/index.ts
@@ -1,43 +1,50 @@
-class Usuario{
-    private _idUsuario: number;
-    private _nome: string;
-    private _telefone: number;
-
-    constructor(id:number, nome:string, telefone:number){
-        this._idUsuario = id;
-        this._nome = nome;
-        this._telefone = telefone;
-    }
-}
-
-class UsuarioRemunerado extends Usuario{
-    private _valorPorVisualizacao: number;
-    private _caixa: number;
-
-    constructor(id:number, nome:string, telefone:number, valorPorVisualizacao:number, caixa:number){
-        super(id, nome, telefone);
-        this._valorPorVisualizacao = valorPorVisualizacao;
-        this._caixa = caixa;
-    }
-
-    sacar(valor:number){
-        this._caixa = this._caixa - valor;
-    }
-}
+import{AplicacaoError, PublicacaoJaCadastradaError, PublicacaoNaoEncontradaError, ValorInvalidoError} from "./excecoes"
 
 abstract class Publicacao{
-    private _idPublicacao: number;
+    private _id: number;
     private _titulo: string;
     private _autor: string;
     private _resumo: string;
     private _qtdPaginas: number;
 
     constructor(id:number, titulo:string, autor:string, resumo:string, qtdPaginas:number){
-        this._idPublicacao = id;
+        this._id = id;
         this._titulo = titulo;
         this._autor = autor;
         this._resumo = resumo;
         this._qtdPaginas = qtdPaginas;
+        this.validarValor(id); this.validarValor(qtdPaginas);
+        this.validarEntrada(titulo); this.validarEntrada(autor); this.validarEntrada(resumo);
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    get titulo(){
+        return this._titulo;
+    }
+
+    get autor(){
+        return this._autor;
+    }
+
+    get resumo(){
+        return this._resumo;
+    }
+
+    private validarValor(valor: number): boolean{
+        if (isNaN(valor) || valor < 0){
+            throw new ValorInvalidoError(`\nO valor ${valor} não é válido! Por favor, verifique os campos preenchidos.`);
+        }
+        return true;
+    }
+
+    private validarEntrada(valor: string): boolean{
+        if(!valor){
+            throw new ValorInvalidoError('\nNão são aceitos valores vazios! Por favor, verifique os campos preenchidos.');
+        }
+        return true;
     }
 }
 
@@ -58,3 +65,76 @@ class Artigo extends Publicacao{
         this._palavrasChave = palavras;
     }
 }
+
+
+class Biblioteca{
+    private _publicacoes: Publicacao[] = [];
+
+    publicar(publicacao: Publicacao){
+        try{
+            this.consultarPublicacao(publicacao.id);
+            throw new PublicacaoJaCadastradaError(`A publicação de id ${publicacao.id} já está cadastrada!`);
+        } catch(e:any){
+            if(e instanceof PublicacaoJaCadastradaError){
+                throw e;
+            }
+            this._publicacoes.push(publicacao);
+        }
+    }   
+
+    consultarPublicacao(id: number): Publicacao{
+        let publicacaoProcurada!: Publicacao;
+        for(let i=0; i<this._publicacoes.length; i++){
+            if(this._publicacoes[i].id == id){
+                publicacaoProcurada = this._publicacoes[i];
+            }
+        }
+
+        if(!publicacaoProcurada){
+            throw new PublicacaoNaoEncontradaError(`Publicação de id ${id} não encontrada!`)
+        }
+
+        return publicacaoProcurada;
+    }
+
+   
+    consultarPorIndice(id: number): number{
+        let indiceProcurado: number = -1;
+        for(let i=0; i<this._publicacoes.length; i++){
+            if(this._publicacoes[i].id == id){
+                indiceProcurado = i;
+            }
+        }
+
+        if(indiceProcurado == -1){
+            throw new PublicacaoNaoEncontradaError(`Publicação de id ${id} não encontrada!`)
+        }
+
+        return indiceProcurado;
+    }
+ 
+    excluir(id: number){
+        let indice: number = this.consultarPorIndice(id);
+
+        for(let i=indice; i<this._publicacoes.length; i++){
+            this._publicacoes[i] = this._publicacoes[i+1];
+        }
+        this._publicacoes.pop();
+    }
+
+    listarPublicacoes(): string{
+        let lista = '';
+
+        for(let i=0; i<this._publicacoes.length; i++){
+            lista = lista + 
+            '\nId: ' + this._publicacoes[i].id  + 
+            ' - Título: ' + this._publicacoes[i].titulo +
+            ' - Autor: ' + this._publicacoes[i]. autor +
+            ' - Resumo: ' + this._publicacoes[i].resumo;
+        }
+
+        return lista;
+    }
+}
+
+export{Biblioteca, Publicacao, Livro, Artigo};
